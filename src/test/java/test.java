5b5e330e509f48ee16c67504e38d5d5b7d215325
syntaxTechnologies <mhrbdrddn@gmail.com>
2024-08-11 19:00:51 -0700

    .

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -2,5 +2,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("new project");
         System.out.println("new project class");
+        System.out.println("new project3");
+        System.out.println("new project class44");
     }
 }
---
+++ src/test/java/test2.java
@@ -0,0 +1,8 @@
+public class test2 {
+    public static void main(String[] args) {
+        System.out.println("new pt1");
+        System.out.println("new p class");
+        System.out.println("new1 pr");
+        System.out.println("new prt4");
+    }
+}
